8106407287caae0989540e90c81210affcc2f3dd
Gaurang Mangukiya <36733487+gaurangmangukiya@users.noreply.github.com>
2022-02-13 23:14:27 +0530

    Multi-Tenancy Multi-Database connector and Setup Express Server

---
+++ app.js
@@ -0,0 +1,19 @@
+process.on('uncaughtException', (err) => {
+    console.log('Caught exception: ' + err);
+}).on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
+});
+
+/** load Mongoose */
+const mongooseLoader = require('./loaders/mongoose');
+
+/** load Environment Variables */
+require('dotenv').config();
+
+/** Connect DB and load express */
+Promise.all([mongooseLoader.connect()]).then(() => {
+    require('./loaders/express');
+}).catch((err) => {
+    console.log("[ERROR] [DATABASE FAIL] => ", err);
+    return process.exit(1);
+});
